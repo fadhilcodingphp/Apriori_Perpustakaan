55b34f857c88da27cb07a224089813419e83f754
fadhilcodingphp <muhammadfadhilay22@gmail.com>
2023-06-05 18:31:58 +0800

    revisi 2

---
+++ footer.php
@@ -0,0 +1,11 @@
+<nav class="navbar fixed-bottom navbar-light bg-light">
+    <a class="navbar-brand" href="#">Copyright @Muhammad Fadhil AY</a>
+</nav>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
